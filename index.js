839b10d06e43b055004ffc3d2048135d75502142
John Azzaro <jazzaro@temple.edu>
2018-10-18 17:14:48 -0700

    additions to HTML and JS

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 "use strict";
+
+function addClickHandlerToButton() {
+  $('main').on('click', 'button', function(event) {
+    const setting = {
+      url:"https://dog.ceo/api/breeds/image/random", 
+      type: "GET",
+      dataType: "json",
+      success,
+      error: failure,
+    }
+    // call dog api
+    $.ajax(setting)
+    // you dont return because you dont know how long its going to take
+    // you need to chain stuff together.
+  });
+}
+
+function success(data) {
+  renderImage(data.message);
+}
+
+function failure(error) {
+  console.error(error);
+}
+
+function generateImage(image) {
+  return `<img src="${image}" alt="dog">`;
+}
+
+function renderImage(image) {
+  $('main').append(generateImage(image));
+}
+
+
+$(addClickHandlerToButton);   
