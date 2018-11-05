ee02449058442374f7be0b126d3a0f14321eead6
John Azzaro <jazzaro@temple.edu>
2018-11-04 16:55:12 -0800

    reloaded js files

---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function handleClickButton() {
+  $('main').on('click', '#dogbutton', function(event) {
+    const setting = {
+      url:"https://dog.ceo/api/breeds/image/random", 
+      type: "GET",
+      dataType: "json",
+      success,
+      error: failure,
+    }
+    $.ajax(setting)
+  });
+}
+
+function success(data) {
+  renderImage(data.message);
+}
+
+function failure(error) {
+  console.error(error);
+}
+
+function generateImage(image) {
+  return `<img src="${image}" alt="dog">`;
+}
+
+function renderImage(image) {
+  $('main').append(generateImage(image));
+}
+
+function setUpEventHandlers() {
+    handleClickButton();
+}
+
+function initializeApp() {
+    setUpEventHandlers();
+}
+
+$(initializeApp);
